dfa895fe3df636e7164d1d129222e90870e06607
Richard <richardwang1134@gmail.com>
2023-09-20 00:58:26 +0800

    refactor: edit/course/all components

---
+++ src/app/edit/courses/all/components/AddCourseForm/index.tsx
@@ -0,0 +1,18 @@
+import { SideMenu } from '@/components/SideMenu'
+import { FC } from 'react'
+import { CourseName } from './CourseName'
+import { DateRange } from './DateRange'
+import { Description } from './Description'
+import { Footer } from './Footer'
+import { Users } from './Users'
+
+const AddCourseForm: FC = () => (
+  <SideMenu title="Add Course" className="w-[500px]" Footer={Footer}>
+    <CourseName />
+    <DateRange />
+    <Description />
+    <Users />
+  </SideMenu>
+)
+
+export default AddCourseForm
--- src/app/edit/courses/all/page.tsx
+++ src/app/edit/courses/all/page.tsx
@@ -1,21 +1,11 @@
-import { SideMenu } from '@/components/SideMenu'
 import { FC } from 'react'
-import { CourseName } from './components/CourseName'
-import { DateRange } from './components/DateRange'
-import { Description } from './components/Description'
-import { Footer } from './components/Footer'
-import { TableView } from './components/TableView'
-import { Users } from './components/Users'
+import AddCourseForm from './components/AddCourseForm'
+import { TableView } from './components/CoursesTable'
 
 const Page: FC = () => (
   <div className="flex w-full h-full">
     <TableView />
-    <SideMenu title="Add Course" className="w-[500px]" Footer={Footer}>
-      <CourseName />
-      <DateRange />
-      <Description />
-      <Users />
-    </SideMenu>
+    <AddCourseForm />
   </div>
 )
 
